cb2ac67c388e1532b431c1e9162081edac684fa5
unknown <gutleberb@gmail.com>
2019-04-23 11:15:42 -0500

    tried fixing routes?

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,5 @@
 // Dependencies
 
-var path = require("path");
 var friendsData = require("../data/friends")
 
 
@@ -9,10 +8,10 @@ module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
     })
-    
+
     app.post("/api/friends", function (req, res) {
         console.log(req.body)
-
+        friendsData.push(req.body)
 
         // all the math
         //compare friendsData to req.body
